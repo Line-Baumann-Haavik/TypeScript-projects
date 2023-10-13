92037d580889d8b95b465d5da2cca05f55a21ba9
Line Baumann Haavik <linebaumann.haavik@dfo.no>
2023-10-13 12:15:20 +0200

    tested out some types and type combinations

--- 06-starting-setup/src/app.ts
+++ 06-starting-setup/src/app.ts
@@ -1,1 +1,18 @@
-// Code goes here!
+type Admin = {
+    name: string,
+    privileges: string[]
+}
+
+type Employee = {
+    name: string,
+    startDate: Date
+}
+
+type elevatedEmployee = Admin & Employee;
+
+const test: elevatedEmployee = {name: "Erik", privileges: ["admin"],startDate: new Date()}
+
+type Combinable = number | string;
+type test2 = number | boolean;
+
+type Combined = Combinable & test2;
