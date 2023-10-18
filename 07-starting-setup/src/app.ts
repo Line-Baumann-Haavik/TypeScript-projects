31113955c5326075781d092ed7508d91485927bf
Line Baumann Haavik <lbhaavik@gmail.com>
2023-10-18 14:14:54 +0200

    checked out promises breifly

--- 07-starting-setup/src/app.ts
+++ 07-starting-setup/src/app.ts
@@ -1,3 +1,13 @@
 const names: Array<string> = ["Max", "Minnie", "Donald", "Manu"];
 //names[0].split(" ");
 
+
+// const promise: Promise<number> = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve(10);
+//     }, 1000)
+// });
+
+// const result = promise.then((data) => {
+//     return data+10;
+// })
