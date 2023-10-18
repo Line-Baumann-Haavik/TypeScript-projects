05acde5fa26d1f4dd7bae0cec6c92dc83391a5c2
Line Baumann Haavik <lbhaavik@gmail.com>
2023-10-18 15:48:27 +0200

    checked out generic functions

--- 07-starting-setup/src/app.ts
+++ 07-starting-setup/src/app.ts
@@ -11,3 +11,26 @@ const names: Array<string> = ["Max", "Minnie", "Donald", "Manu"];
 // const result = promise.then((data) => {
 //     return data+10;
 // })
+
+function merge<T extends Object, U>(objA: T, objB: U){
+    return Object.assign(objA, objB);
+}
+
+const mergeObj = merge({name: "test"}, {age: 20});
+console.log(mergeObj); 
+
+interface Lenghty {
+    length: number
+}
+
+function countAndDescribe<T extends Lenghty>(value: T){
+    let descriptionText = "Got no length:(";
+    if(value.length === 1){
+        descriptionText = "Got a length of 1 elements";
+    } else if (value.length > 1){
+        descriptionText = "Got a length of "+value.length+" elements";
+    }
+    return [value, descriptionText];
+}
+
+console.log(countAndDescribe("Hi there!"));
