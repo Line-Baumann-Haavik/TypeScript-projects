184a3e6956df4329cc98b6f5d08c13590ad23346
Line Baumann Haavik <lbhaavik@gmail.com>
2023-10-20 16:09:35 +0200

    added initial code to show form, moved css file to correct place

--- 09-initial-starting-setup/src/app.ts
+++ 09-initial-starting-setup/src/app.ts
@@ -1,1 +1,19 @@
-// Code goes here!
+class ProjectInput {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLFormElement;
+
+    constructor(){
+        this.templateElement = document.getElementById("project-input")! as HTMLTemplateElement;
+        this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+        const importedContent = document.importNode(this.templateElement.content, true);
+        this.element = importedContent.firstElementChild! as HTMLFormElement;
+        this.attach();
+    }
+    private attach(){
+        this.hostElement.insertAdjacentElement("afterbegin", this.element);
+    }
+}
+
+const project1 = new ProjectInput();
